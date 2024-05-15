966a0095843fa3a9cacb5041473ca2f0d522eead
loqumi <loqumihh@gmail.com>
2024-05-15 14:39:05 +0200

    mobile home page fixes

--- ui/atoms/TextLine.tsx
+++ ui/atoms/TextLine.tsx
@@ -12,7 +12,7 @@ export const TextLine = ({ children, rotation, ...props }: TextLineProps) => (
       boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
       display: { xs: "none", lg: "block" },
       right: { lg: "-40%", xl: "-2%" },
-      top: { lg: "20%", xl: "20%" },
+      top: { lg: "25vh", xl: "20vh" },
     }}
     fontSize={30}
     color="black"
